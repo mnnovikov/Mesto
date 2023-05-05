e4727adb57ace6ea67bb83aefc3cf9e366e5665a
Maksim Novikov <a3657952@yandex.ru>
2023-05-05 14:31:02 +0300

    first commit

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
+let nameImput = document.querySelector('.popup__input_type_name');
+let jobImput = document.querySelector('.popup__input_type_job');
+let saveData = document.querySelector('.popup__container');
+let showName = document.querySelector('.lead__title');
+let showJob = document.querySelector('.lead__subtitle');
+
 let openModalButton = document.querySelector('.lead__edit-button'),
   modal = document.querySelector('.popup'),
-  closeModalButton = document.querySelectorAll('.popup__close');
+  closeModalButton = document.querySelectorAll('.popup__button-close');
+
+function openModal() {
+  modal.classList.add('popup__opened');
+  nameImput.value = showName.textContent;
+  jobImput.value = showJob.textContent;
+}
+
+function closeModal() {
+  modal.classList.remove('popup__opened');
+}
+
+function saveModal(event) {
+  event.preventDefault();
+  showName.innerHTML = nameImput.value;
+  showJob.innerHTML = jobImput.value;
+  closeModal();
+}
+
+saveData.addEventListener('submit', saveModal);
+
+// document.addEventListener('click', function(event) {
+//   let pushLikeButton = event.target.closest('.photo-grid__button');
+//   if (pushLikeButton) {
+//     pushLikeButton.classList.toggle('photo-grid__button_black');
+//   }
+// });
 
 openModalButton.addEventListener('click', openModal);
 
 closeModalButton.forEach(closeBtn => {
   closeBtn.addEventListener('click', closeModal);
-});
-
-function openModal() {
-  modal.classList.add('popup_opened');
-}
-
-function closeModal() {
-  modal.classList.remove('popup_opened');
-}
-
-let nameImput = document.querySelector('.popup__input-name');
-let jobImput = document.querySelector('.popup__input-job');
-let saveData = document.querySelector('.popup__button');
-let showName = document.querySelector('.lead__title');
-let showJob = document.querySelector('.lead__subtitle');
-
-saveData.addEventListener('click', function() {
-  showName.innerHTML = nameImput.value;
-  showJob.innerHTML = jobImput.value;
-  closeModal();
-});
-
-
-document.addEventListener('click', function(event) {
-  let pushLikeButton = event.target.closest('.photo-grid__button');
-  if (pushLikeButton) {
-    pushLikeButton.classList.toggle('photo-grid__button-black');
-  }
 });
